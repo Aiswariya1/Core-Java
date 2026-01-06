22addb76f026e10559f7e7dec2bda5c55efa725c
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-06 13:54:16 +0530

    add

---
+++ variables/instance/program 02.java
@@ -0,0 +1,14 @@
+// Multiple instance variables
+
+package variables.instance;
+class Test{
+  int num1=10;
+  int num2=20;
+  int num3=30;    
+    public static void main(String[]args){
+       Test t1=new Test();
+          System.out.println(t1.num1); //10
+          System.out.println(t1.num2); //20
+          System.out.println(t1.num3); //30
+    }
+}
