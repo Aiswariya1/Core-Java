17fbb8f32400660599e44ca104e17723914d251d
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-07 17:08:26 +0530

    add

---
+++ Typecasting/explicit.java
@@ -0,0 +1,10 @@
+   // program
+
+class Test{
+    public static void main(String[]args){
+       int x=10;
+       //explicit type casting
+       byte b=(byte) x;
+        System.out.println(b); //10
+    }
+}
