17d45da8a19c5ff87478ada3641aec23090e51f6
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-14 22:47:08 +0530

    add

---
+++ Operators/binary/Equality/Primitive.java
@@ -0,0 +1,34 @@
+//Example-1
+
+package binary.Equality;
+
+public class Primitive {
+    public static void main(String[]args){
+        int x =10;
+        int y =20;
+       
+        System.out.println(x==y); //false
+        int z =20;
+        
+        System.out.println(y==z); //true
+    }
+}
+
+//ex-2
+
+class Test{
+    public static void main(String[]args){
+        double d1 =1.5;
+        double d2 =1.5;
+        System.out.println(d1==d2); //true
+        
+    }
+}
+
+//ex-3
+
+class Test1{
+    public static void main(String[] args) {
+        boolean b1 
+    }
+}
