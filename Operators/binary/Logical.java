f921af5f60de98227318d264c3f91ab24916ceb2
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-16 21:08:06 +0530

    add

--- Operators/binary/Logical.java
+++ Operators/binary/Logical.java
@@ -1,3 +1,17 @@
-public class Logical {
+// 1. Logical AND(&&)
+// All conditions must be true
+
+public class Logical{
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
     
-}
+    if(a < b && b > 15 ){
+        System.out.println(1);
+    } else{
+        System.out.println(0);
+     }
+  } 
+    }
+  
+
