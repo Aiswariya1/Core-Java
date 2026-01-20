5778c58e8f62256788cd99748af5cf8fa29f5611
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-20 17:48:01 +0530

    add

--- variables/static/Sample2.java
+++ variables/static/Sample2.java
@@ -1,18 +1,18 @@
     // Access it in 3 common ways:-
 
     class Test{
-    static int x=10;
+    static int y=10;
     public static void main(String[]args){
 
       //by directly
-         System.out.println(x); //10
+         System.out.println(y); //10
 
       //by classname
-         System.out.println(Test.x); //10
+         System.out.println(Test.y); //10
 
       //by object reference
          Test t1=new Test();
-         System.out.println(t1.x); //10
+         System.out.println(t1.y); //10
 
     }
 }
