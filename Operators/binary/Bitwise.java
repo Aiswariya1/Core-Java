63150493241ce65bffad7f07dee015df9f7bfc74
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-25 18:39:52 +0530

    add

--- Operators/binary/Bitwise.java
+++ Operators/binary/Bitwise.java
@@ -17,7 +17,7 @@ class Bitwise{
 
 //program
 
-class Demo{
+class Bitwise1{
     public static void main(String[] args) {
         System.out.println(true | true); //true
         System.out.println(true | false); // true
