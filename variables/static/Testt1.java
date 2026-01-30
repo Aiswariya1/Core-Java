b87880a488bf1732bf08cda1c7f3deeef3641a1e
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-30 11:07:27 +0530

    add

---
+++ variables/static/Testt1.java
@@ -0,0 +1,17 @@
+   // it shares same copy to every objects.
+
+   public class Testt1 {
+      static int x = 10;
+    public static void main(String[]args){
+        
+          System.out.println(Testt1.x); //10
+          System.out.println(Testt1.x); //10
+
+            Testt1.x=20;
+
+          System.out.println(Testt1.x); //20
+          System.out.println(Testt1.x); //20
+    }
+   }
+   
+        
