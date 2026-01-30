276e8a6f373e6c28d2d8555fa651e373c07efb5d
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-30 11:07:10 +0530

    add

---
+++ variables/static/Testt.java
@@ -0,0 +1,11 @@
+  // it can be declared inside the class, outside any method, block or constructor.
+
+public class Testt{
+     static int x=10; // static variable
+     
+    public static void main(String[]args){
+         System.out.println(x); //10
+         System.out.println(y); //20
+    }
+    static int y=20;
+}                           
