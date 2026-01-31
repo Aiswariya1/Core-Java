b6f1bb6c763331348d95d9d42119563971adc017
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-01-31 12:42:32 +0530

    add

---
+++ variables/static/Testt2.java
@@ -0,0 +1,19 @@
+    // Access it in 3 common ways:-
+
+    public class Testt2 {
+    static int y=10;
+    public static void main(String[]args){
+
+      //by directly
+         System.out.println(y); //10
+
+      //by classname
+         System.out.println(Testt2.y); //10
+
+      //by object reference
+         Testt2 t1=new Testt2();
+         System.out.println(t1.y); //10
+
+    }
+}
+
