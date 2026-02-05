4b0e1c3d814db2dff9b34b5003dca87dcb0ddcbb
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-05 13:10:23 +0530

    add

---
+++ FlowControl/Selective/Else-if/StudentPercentage.java
@@ -0,0 +1,30 @@
+public class StudentPercentage{
+    public static void main(String[]args){
+
+        int per=85;
+
+        if(per <= 100 && per >= 91){
+            System.out.println("A+");
+        }
+        else if(per <= 90 && per >= 81){
+            System.out.println("A");
+        }
+        else if(per <= 80 && per >= 71){
+            System.out.println("B+");
+        }
+        else if(per <= 70 && per >= 61){
+            System.out.println("B");
+        }
+        else if(per <= 60 && per >= 51){
+            System.out.println("C");
+        }
+        else if(per <= 50 && per >= 33){
+            System.out.println("D");
+        }
+        else{
+            System.out.println("Fail");
+        }
+
+    }
+
+}
