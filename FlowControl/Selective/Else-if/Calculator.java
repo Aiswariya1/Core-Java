46d81fdb1bba7a2c436a765ae5ee8e230d20dff0
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-06 13:27:09 +0530

    add

---
+++ FlowControl/Selective/Else-if/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+        char op = '+';
+
+        if(op == '7'){
+            System.out.println("Sum is" + (x+y));
+        }
+            else if(op == '-'){
+                System.out.println("Sub is" + (x-y));
+            }
+
+            else if(op == '*'){
+                System.out.println("Prod is" + (x*y));
+            }
+
+            else{
+                 System.out.println("Invalid op");
+           }
+
+         }
+     }
+
+
+    
+    
+
