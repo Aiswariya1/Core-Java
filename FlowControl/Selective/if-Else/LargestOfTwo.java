39e0264f1294fe7d4cb82ed69fcc7f1abf68dd27
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-07 13:40:17 +0530

    add

---
+++ FlowControl/Selective/if-Else/LargestOfTwo.java
@@ -0,0 +1,17 @@
+//
+
+public class LargestOfTwo {
+  public static void main(String[] args) {
+    
+    int a = 28;
+    int b = 82;
+
+    if(a > b){
+        System.out.println("Largest Number is" + a);
+    } else{
+        System.out.println("Largest Number is" + b);
+    }
+  }  
+}
+
+
