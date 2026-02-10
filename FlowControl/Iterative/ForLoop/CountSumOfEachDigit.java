fad1556be949f3d4fb9ca648f0c6a85a7aea997d
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-10 15:36:35 +0530

    add

--- FlowControl/Iterative/ForLoop/CountSumOfEachDigit.java
+++ FlowControl/Iterative/ForLoop/CountSumOfEachDigit.java
@@ -8,13 +8,9 @@ public static void main(String[] args) {
     for(int num = 1292; num > 0; num = num/10){
         rem = num % 10;
         sum = sum + rem;
-
-
     }
 
- 
-}
-
-
+    System.out.println(sum);
     
+   }  
 }
