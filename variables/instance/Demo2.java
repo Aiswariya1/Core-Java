2a437c0353e2600c097110c3d201ae99d3b96958
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-12 12:19:55 +0530

    add

--- variables/instance/Demo2.java
+++ variables/instance/Demo2.java
@@ -1,7 +1,6 @@
 // it is not mandatory to initialise the non-static variable, the default value is given by JVM according to the datatypes.
 
-
- class Demo2 {
+public class Demo2 {
     int num1; //instance variable(not initialized)
     public static void main(String[]args){
         Demo2 t1 = new Demo2();
