f1dc4b79d530e885318a7b0eaf07e16aac681b16
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-15 18:15:52 +0530

    add

--- Operators/binary/Relational.java
+++ Operators/binary/Relational.java
@@ -1,6 +1,6 @@
 //program
 
- class Relational{
+ public class Relational{
    public static void main(String[]args){
        System.out.println(10<10); //false
         System.out.println(10>9); //true
