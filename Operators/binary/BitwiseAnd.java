4bdd80792fabc574acfd08d92eaff13adebebb54
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-16 13:39:17 +0530

    add

---
+++ Operators/binary/BitwiseAnd.java
@@ -0,0 +1,40 @@
+// It is like &, | or ^
+
+// 1. (a & b)- it returns true if both  the operands are true.
+
+//program
+
+public class BitwiseAnd{
+    public static void main(String[] args) {
+        System.out.println(true & true); //true
+        System.out.println(true & false); //false
+        System.out.println(false & false); //false
+        System.out.println(false & true); //false
+    }
+}
+
+// 2. (a | b)- it returns true if any one of the operands is true.
+
+//program
+
+class BitwiseOr{
+    public static void main(String[] args) {
+        System.out.println(true | true); //true
+        System.out.println(true | false); // true
+        System.out.println(false | false); //false
+        System.out.println(false | true); //true
+    }
+}
+
+// 3. (a ^ b)- it returns true if any both  the operands are different.
+
+//program
+
+class BitwiseXor{
+    public static void main(String[] args) {
+        System.out.println(true ^ true); //false
+        System.out.println(true ^ false); //true
+        System.out.println(false ^ false); //false
+        System.out.println(false ^ true); //true
+    }
+}
