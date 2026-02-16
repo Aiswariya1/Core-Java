40d4e8d3c036908b85a6b0c34f081f53e29eda23
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-16 13:39:26 +0530

    add

---
+++ Operators/binary/LogicalAnd.java
@@ -0,0 +1,32 @@
+// 1. Logical AND(&&)
+// All conditions must be true
+
+ public class LogicalAnd{
+    public static void main(String[] args) {
+        int a =10;
+        int b =20;
+    
+    if(a < b && b > 15 ){
+        System.out.println(1); //1
+    } else{
+        System.out.println(0);
+     }
+  } 
+    }
+  
+
+// 2. Logical OR(||)
+// At least one condition must be true
+
+ class LogicalOr {
+    public static void main(String[] args) {
+        int a =10;
+        int b =5;
+    
+    if(a > b || b > 10 ){
+        System.out.println(1); //1
+    } else{
+        System.out.println(0);
+     }
+  } 
+    }
