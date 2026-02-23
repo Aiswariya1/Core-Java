ab780d911212d80e992e8c54c3eade2f5d0a01ee
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-23 14:33:22 +0530

    add

--- Operators/binary/Assignment/program.java
+++ Operators/binary/Assignment/program.java
@@ -7,6 +7,7 @@ class ChainedAndCompound{
     int a,b,c,d;
     a = b = c = d=20;
     a += b-= c*=d /= 2;
+    
     System.out.println(a  + "" + b + "" + c + "" + d); //-160, -180, 200, 10
        }
     }
