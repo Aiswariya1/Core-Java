208bb45ef71e7fe175c90e0a831778e47e16924d
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-26 11:48:22 +0530

    add

--- Operators/binary/BitwiseAnd.java
+++ Operators/binary/BitwiseAnd.java
@@ -5,6 +5,7 @@
 //program
 
 public class BitwiseAnd{
+    
     public static void main(String[] args) {
         System.out.println(true & true); //true
         System.out.println(true & false); //false
