198cd72304f5a61095ee5c320e6cf48c5127fc45
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-02-28 14:11:09 +0530

    add

--- Operators/ternary.java
+++ Operators/ternary.java
@@ -29,8 +29,9 @@
 
 //ex-2
 class Ternary1 {
+    
     public static void main(String[]args){
-        
+
         int a = 10, b = 20, c = 15;
 
           int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
