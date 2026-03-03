ae1f2315eb0e4957b1bafd8e47e415332badd669
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-03 10:33:24 +0530

    add

--- Typecasting/implicit.java
+++ Typecasting/implicit.java
@@ -1,7 +1,8 @@
 //class implicit {
     public class Implicit{
-        
+
     public static void main(String[]args){
+        
         char ch='a';
         int x= ch; //implicit casting
         System.out.println(x); //97
