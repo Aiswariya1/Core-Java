3e59c1ac7a42423c48491bd9d884cc8886a05ef1
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-05 22:54:11 +0530

    add

--- Operators/binary/Assignment/Simple.java
+++ Operators/binary/Assignment/Simple.java
@@ -1,12 +1,9 @@
 //program
 
 class Simple{
-    
-    public static void main(String[]args){
-        
+    public static void main(String[]args){  
         int x;
         x=10; //assigning value 10 to variable x
         System.out.println(x); //10
     }
-
 }
