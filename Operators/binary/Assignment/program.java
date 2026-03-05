7925aaacb2dee2fd4cdceb193a6ff0d4b891aeba
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-05 22:56:38 +0530

    add

--- Operators/binary/Assignment/program.java
+++ Operators/binary/Assignment/program.java
@@ -2,16 +2,15 @@
  //example of chained + compound assingnment.
 
 class ChainedAndCompound{
-   
     public static void main(String[]args){
-
     int a,b,c,d;
     a = b = c = d=20;
     a += b-= c*=d /= 2;
     
     System.out.println(a  + "" + b + "" + c + "" + d); //-160, -180, 200, 10
-       }
-    }
+      
+     }
+ }
 
     //Evaluation from Right to Left :-
       // d/2 --> d=d/2 --> 10
