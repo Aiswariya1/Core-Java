13fe7aab55e39c7a7712869804d73e75caef2476
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-13 11:14:18 +0530

    add

--- Operators/binary/Assignment/Chained.java
+++ Operators/binary/Assignment/Chained.java
@@ -3,9 +3,10 @@
  public class Chained{
     
     public static void main(String[]args){
-        
+
         int a,b,c;
         a=b=c=10; //chained assignment
+        
         System.out.println(a); //10
         System.out.println(b); //10
         System.out.println(c); //10
