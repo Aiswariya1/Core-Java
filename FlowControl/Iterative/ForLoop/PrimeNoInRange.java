a050b4dd75c16517ccec979df63daa570d2eb10a
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-21 19:57:48 +0530

    add

--- FlowControl/Iterative/ForLoop/PrimeNoInRange.java
+++ FlowControl/Iterative/ForLoop/PrimeNoInRange.java
@@ -3,7 +3,7 @@
 public class PrimeNoInRange {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);  
-        System.out.println("Enter the no")
+        System.out.println("Enter the no");
         int n=sc.nextInt();
 
         for(int i=2; i<=n; i++){
