a4b7bef36af57986a61549547140a35c7395155a
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-22 19:23:17 +0530

    add

--- FlowControl/Iterative/ForLoop/CountTheDigit.java
+++ FlowControl/Iterative/ForLoop/CountTheDigit.java
@@ -3,9 +3,7 @@
  public class CountTheDigit {
     
     public static void main(String[] args) {
-
         int count = 0;
-
         for( int num = 8092; num>0; num = num / 10){
              count++;
         }
