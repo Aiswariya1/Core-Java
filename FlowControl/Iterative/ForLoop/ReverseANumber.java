552232b23505e91a732760839ecbe10407b05391
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-26 21:51:29 +0530

    add

--- FlowControl/Iterative/ForLoop/ReverseANumber.java
+++ FlowControl/Iterative/ForLoop/ReverseANumber.java
@@ -3,7 +3,6 @@
 public class ReverseANumber {
 
     public static void main(String[] args) {
-
         int reverse = 0;
         for(int num = 1234; num > 0; num = num / 10){
             int rem= num % 10;
