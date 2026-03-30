d02bd7b7c2da78a82a20c758120fcef59ea1a673
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-03-30 21:40:37 +0530

    add

--- FlowControl/Iterative/ForLoop/CountSumOfEachDigit.java
+++ FlowControl/Iterative/ForLoop/CountSumOfEachDigit.java
@@ -2,10 +2,8 @@
 
 public class CountSumOfEachDigit {
 public static void main(String[] args) {
-    
     int sum = 0;
     int rem = 0;
-
     for(int num = 1292; num > 0; num = num/10){
         rem = num % 10;
         sum = sum + rem;
