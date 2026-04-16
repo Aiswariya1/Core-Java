6495b875d3cf80dcf43313b916b2862d6cd08709
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-04-16 21:42:26 +0530

    add

--- FlowControl/Iterative/ForLoop/PrimeNoInArray.java
+++ FlowControl/Iterative/ForLoop/PrimeNoInArray.java
@@ -2,12 +2,18 @@ public class PrimeNoInArray {
 
     public static void main(String[] args) {
         int[] arr={2,5,8,3,9,74,24};
-    for(int i=0; i>arr.length, i++){
+    for(int i=0; i< arr.length; i++)
+        {
         int num=arr[i];
         int count=0;
-    for
-
+    
+    for(int j=1; j<=num; j++){
+        if(num%j==0){
+            count++;
+        }
+    }
+if
     
     }
-    }
+}
 }
