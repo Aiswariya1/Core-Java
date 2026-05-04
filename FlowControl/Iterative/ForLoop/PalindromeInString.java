f4e218b105a83c1679fbb411ce63653a1fce8bed
Aiswariya Pradhan <aiswariyapradhan2@gmail.com>
2026-05-04 16:35:03 +0530

    add

--- FlowControl/Iterative/ForLoop/PalindromeInString.java
+++ FlowControl/Iterative/ForLoop/PalindromeInString.java
@@ -5,6 +5,9 @@ public class PalindromeInString {
         String str="Mom";
         String rev=" ";
 
-        
+        for(int i=str.length()-1;  i>=0; i++ ){
+            rev= rev+str.charAt(i);
+        }
+        if()
     }
 }
